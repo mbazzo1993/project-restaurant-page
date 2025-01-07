da12f1dd0a5af0694a032076e7804a278369d7a1
mbazzo1993 <matthewbazzo@gmail.com>
2025-01-07 14:09:26 -0500

    feat: add content to home page

--- src/home.js
+++ src/home.js
@@ -1,11 +1,25 @@
 
-const headerText1 = 'Welcome to';
-const headerText2 = 'Porca Miseria';
-const headerText3 = 'Purveyors of Italian pork products since 1869';
+const bannerText1 = 'Welcome to';
+const bannerText2 = 'Porca Miseria';
+const bannerText3 = 'Purveyors of Italian pork products since 1869';
+const descHeaderText = 'About Us';
 const descText = "At Porca Miseria, we take pride in our authentic Italian pork products, which have been perfected after years of dedication to the craft of Italian butchery and deli meats. In addition to our wide selection of delicious product, we also offer courses in butchery and curing so that you can create your own products at home."
-
+const hoursHeaderText = 'Hours';
+const hoursTextArr = [
+    "Monday: 10am - 10pm",
+    "Tuesday: 10am - 10pm",
+    "Wednesday: 10am - 10pm",
+    "Thursday: 10am - 10pm",
+    "Friday: 10am - 10pm",
+    "Saturday: 10am - 10pm",
+    "Sunday: 12pm - 6pm"
+]
+const locationHeaderText = "Location";
+const locationText = '123 Fake Street, Toronto, Ontario';
 
 export function loadHomePage() {
+
+    // construct banner and content
     const bannerElem = document.createElement('div');
     bannerElem.classList.add("banner");
 
@@ -15,31 +29,57 @@ export function loadHomePage() {
     const headerElem = document.createElement('div');
     headerElem.classList.add('header-container');
 
-    const headerText1Elem = document.createElement('p');
-    headerText1Elem.innerText = headerText1;
+    const bannerText1Elem = document.createElement('p');
+    bannerText1Elem.innerText = bannerText1;
 
-    const headerText2Elem = document.createElement('div');
-    headerText2Elem.classList.add('header');
-    headerText2Elem.innerText = headerText2;
+    const bannerText2Elem = document.createElement('div');
+    bannerText2Elem.classList.add('header');
+    bannerText2Elem.innerText = bannerText2;
 
-    const headerText3Elem = document.createElement('p');
-    headerText3Elem.innerText = headerText3;
+    const bannerText3Elem = document.createElement('p');
+    bannerText3Elem.innerText = bannerText3;
 
-    headerElem.appendChild(headerText1Elem);
-    headerElem.appendChild(headerText2Elem);
-    headerElem.appendChild(headerText3Elem);
+    headerElem.appendChild(bannerText1Elem);
+    headerElem.appendChild(bannerText2Elem);
+    headerElem.appendChild(bannerText3Elem);
 
     imageElem.appendChild(headerElem);
 
     bannerElem.appendChild(imageElem);
 
+    // construct description and content
     const descriptionElem = document.createElement('div');
     descriptionElem.classList.add('desc');
+
+    const descHeaderElem = document.createElement('h2');
+    descHeaderElem.innerText = descHeaderText;
 
     const descriptionTextElem = document.createElement('p');
     descriptionTextElem.innerText = descText;
 
+    const hoursHeaderElem = document.createElement('h2');
+    hoursHeaderElem.innerText = hoursHeaderText;
+
+    const hoursUListElem = document.createElement('ul');
+    let hoursText = '';
+    for (hoursText of hoursTextArr) {
+        const hoursLItemElem = document.createElement('li');
+        hoursLItemElem.innerText = hoursText;
+        hoursUListElem.appendChild(hoursLItemElem);
+    }
+
+    const locationHeaderElem = document.createElement('h2');
+    locationHeaderElem.innerText = locationHeaderText;
+
+    const locationTextElem = document.createElement('p');
+    locationTextElem.innerText = locationText;
+
+    descriptionElem.appendChild(descHeaderElem);
     descriptionElem.appendChild(descriptionTextElem);
+    descriptionElem.appendChild(hoursHeaderElem);
+    descriptionElem.appendChild(hoursUListElem);
+    descriptionElem.appendChild(locationHeaderElem);
+    descriptionElem.appendChild(locationTextElem);
 
 
     return {bannerElem, descriptionElem};
